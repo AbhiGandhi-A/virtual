b6f990e2a793e4f7581234c327a8b0f1774e7bc8
ABHIGANDHI <abhigandhi918@gmail.com>
2025-11-02 20:35:57 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"
+const API_URL = import.meta.env.VITE_API_URL || "https://virtual-1-8hpp.onrender.com"
 
 const api = axios.create({
   baseURL: API_URL,
